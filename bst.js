44d1f8440edd234d1a084cd8410a9f8199c43d97
Bhuvnesh Gupta <bhuvneshgupta333@gmail.com>
2018-03-31 15:52:04 +0530

    bst operations

--- bst.js
+++ bst.js
@@ -1,64 +1,199 @@
-function Node(val){
-  this.value = val;
-  this.left = null;
-  this.right = null;
+function binarySearchTree(){
+	this.root = null;
+	this.max_height = 0;
+	this.min_value = -1;
 }
 
-function BinarySearchTree(){
-  this.root = null;
+function newNode(val){
+	this.value = val;
+	this.left  = null;
+	this.right = null;
 }
 
-BinarySearchTree.prototype.push = function(currentNode,val){
-	var newNode = new Node(val);
+binarySearchTree.prototype.insert = function(currentNode,val){
+	var node = new newNode(val);
 
-	if(!currentNode){
-		this.root = newNode;
+	if(this.root == null){
+		this.root = node;
 		return;
-	}
+	}	
 
 	if(val < currentNode.value){
-		if(!currentNode.left) {
-			currentNode.left=newNode;
-			//console.log(currentNode)
+		if(currentNode.left == null){
+			currentNode.left = node;
 			return;
 		}
 		else{
-			this.push(currentNode.left,val);
+			this.insert(currentNode.left,val);
 		}
 	}
 	else{
-		if(!currentNode.right) {
-			currentNode.right=newNode;
-			//console.log(currentNode)
+		if(currentNode.right == null){
+			currentNode.right = node;
 			return;
 		}
 		else{
-			this.push(currentNode.right,val);
-		}
+			this.insert(currentNode.right,val);
+		}	
 	}
 }
 
-var bst = new BinarySearchTree();
+binarySearchTree.prototype.height = function(node,current_height){
+	if(this.root == null){
+		this.max_height = 0;
+		return;
+	}
 
-/*console.log(bst.root)
-console.log('======')
-bst.push(bst.root,100);
+	if(current_height > this.max_height){
+		this.max_height = current_height;
+	}
 
-console.log(bst.root)
-console.log('======')
-bst.push(bst.root,70);
+	console.log("\n\n\n node   ",JSON.stringify(node))
 
-console.log(bst.root)
-console.log('======')
-bst.push(bst.root,104);
+	if(node.left!=null){
+		console.log("\n\n\n left called  ",JSON.stringify(node.left))
+		this.height(node.left,current_height+1);
+	}
 
-console.log(bst.root)
-console.log('======')
-bst.push(bst.root,107);
+	if(node.right!=null){
+		console.log("\n\n\n right  called ",JSON.stringify(node.right))
+		this.height(node.right,current_height+1);
+	}
+}
 
-console.log(bst.root)
-console.log('======')
-bst.push(bst.root,109);
+/*binarySearchTree.prototype.remove = function(node){
+	if(this.root == null)
+	{
+		return null;
+	}
 
-console.log(JSON.stringify(bst.root))*/
+	if(node.value == currentNode.value){
+		currentNode.value = null;
+		currentNode.left = null;
+		currentNode.right = null;
+
+		if(currentNode.left!=null){
+			currentNode.left 
+		}
+		if(currentNode.right!=null){
+			currentNode.left 
+		}
+	}
+}*/
+
+binarySearchTree.prototype.dfs = function(currentNode){
+	if(this.root == null)
+		return;
+
+	if(currentNode.value){
+		console.log(currentNode.value);
+	}
+
+	if(currentNode.left){
+		this.dfs(currentNode.left);
+	}
+	if(currentNode.right){
+		this.dfs(currentNode.right);
+	}
+}
+
+binarySearchTree.prototype.bfs = function(currentNode){
+	if(this.root == null)
+		return;
+
+	if(currentNode.value){
+		console.log(currentNode.value);
+	}
+
+	if(currentNode.left){
+		this.dfs(currentNode.left);
+	}
+	if(currentNode.right){
+		this.dfs(currentNode.right);
+	}
+}
+
+binarySearchTree.prototype.findMinValue = function(currentNode){
+	if(this.root == null)
+		return;
+
+	if( (currentNode.value < this.min_value) || this.min_value == -1 ){
+		this.min_value = currentNode.value;
+	}
+
+	if(currentNode.left!=null) {
+		this.findMinValue(currentNode.left);
+	}
+}
+
+binarySearchTree.prototype.searchValue = function(currentNode,val){
+	if(!currentNode){
+		return -1;
+	}
+
+	if(val < currentNode.value){
+		return this.searchValue(currentNode.left,val);
+	}
+	else if(val > currentNode.value){
+		return this.searchValue(currentNode.right,val);
+	}
+	else{
+		return 1;
+	}
+}
+
+binarySearchTree.prototype.commonAncestor = function(currentNode,n1,n2){
+	if(!currentNode)
+		return -1;
+
+	var current_value = currentNode.value;
+	if(n1 < current_value && n2 < current_value){
+		return this.commonAncestor(currentNode.left,n1,n2);
+	}
+
+	if(n1 > current_value && n2 > current_value){
+		return this.commonAncestor(currentNode.right,n1,n2);
+	}
+
+	return current_value;
+
+
+}
+
+var bst = new binarySearchTree();
+
+console.log("\n", JSON.stringify(bst.root));
+console.log("\n height of a tree before pushing = ",bst.max_height);
+
+bst.insert(bst.root,100);
+console.log("\n", JSON.stringify(bst.root));
+
+bst.insert(bst.root,60);
+console.log("\n", JSON.stringify(bst.root));
+
+bst.insert(bst.root,130);
+console.log("\n", JSON.stringify(bst.root));
+
+bst.insert(bst.root,70);
+console.log("\n", JSON.stringify(bst.root));
+
+bst.insert(bst.root,80);
+console.log("\n", JSON.stringify(bst.root));
+
+bst.insert(bst.root,67);
+console.log("\n", JSON.stringify(bst.root));
+
+
+bst.height(bst.root,bst.max_height);
+console.log("\n height of a tree after all nodes= ",bst.max_height);
+
+console.log("\n\n dfs = ",bst.dfs(bst.root));
+
+bst.findMinValue(bst.root)
+console.log("\n\n minimum value = ",bst.min_value);
+
+console.log("\n\n search value 70 ? ",bst.searchValue(bst.root,70) == 1 ? 'yes':'no');
+
+console.log("\n\n commonAncestor of 80 and 67 is : ",bst.commonAncestor(bst.root,80,67));
+
 
